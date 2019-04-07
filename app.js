47f3849c5c6fdf4eabece2288eb49fca8bde873c
Mike Mullen <mmullen227@gmail.com>
2019-04-07 00:18:51 -0400

    take command line city from user

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    if(error){
-        return console.log('Error: ', error);
-    } else {
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
-            if(error) {
-                return console.log('Error', error)
-            }
-            
-            console.log(data.location)
-            console.log(forecastData)
-        })
-    }
+var userCity;
+const command = process.argv[2];
+const command2 = process.argv[3];
+if(process.argv.length > 3) {
+     userCity = command + ' ' + command2
+} else {
+     userCity = command;
+}
 
+
+
+if(userCity) {
+    geocode(userCity, (error, data) => {
+        if(error){
+            return console.log('Error: ', error);
+        } else {
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if(error) {
+                    return console.log('Error', error)
+                }
+                
+                console.log(data.location)
+                console.log(forecastData)
+            })
+        }    
+    })
     
-})
+} else {
+    console.log('please enter a city name')
+}
 
